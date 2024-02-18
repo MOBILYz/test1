9f360fd2c7a7c6bed9c71371e0eafed7e2c50aaf
MOBILYz <160411427+MOBILYz@users.noreply.github.com>
2024-02-18 22:40:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementById('yes').addEventListener('click', function() {
-    document.getElementById('cartoon').src = 'images/dog1.gif'; /* เปลี่ยนรูปเมื่อคลิก yes */
+    document.getElementById('cartoon').src = 'dog1.gif'; /* เปลี่ยนรูปเมื่อคลิก yes */
     document.getElementById('text').textContent = 'รักแฟนนนน'; /* เปลี่ยนข้อความเมื่อคลิก yes */
 
   });
   
   document.getElementById('no').addEventListener('click', function() {
-    document.getElementById('cartoon').src = 'images/dog2.gif'; /* เปลี่ยนรูปเมื่อคลิก no */
+    document.getElementById('cartoon').src = 'dog2.gif'; /* เปลี่ยนรูปเมื่อคลิก no */
     document.getElementById('text').textContent = 'โป้งแล้วว'; /* เปลี่ยนข้อความเมื่อคลิก yes */
 
   });
